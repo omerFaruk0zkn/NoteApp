bf5a9259134924e6ae58c1bd2b4ca0f4b8003a7e
Omer Faruk Ozkan <faruk_s.v.i@hotmail.com>
2024-12-24 19:23:06 +0300

    Update Program.cs

--- NoteAppAPI/Program.cs
+++ NoteAppAPI/Program.cs
@@ -38,7 +38,7 @@ builder.Services.AddSwaggerGen(options =>
 });
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddCors(options =>
 {
